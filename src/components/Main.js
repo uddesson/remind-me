d1a90fc7af1da413ec276b80ba60bf6ee96319da
Emma Uddesson <emma.uddesson@gmail.com>
2018-07-27 15:19:21 +0200

    Add draft for modal component
    Importing + using react-native-modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import { ScrollView } from 'react-native';
 import Header from './Header';
 import List from './List';
+import AddModal from '../components/modals/AddModal';
 
 export default class Main extends Component {
   render() {
     return (
-     <ScrollView>
-      <Header />
-      <List />
-     </ScrollView>
+      <>
+        <AddModal />
+        <ScrollView>
+          <Header />
+          <List />
+        </ScrollView>
+     </>
     );
   }
 }
