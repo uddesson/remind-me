69658d05bd5c044eaa664e8a7071a49dd2a2c9a7
Emma Uddesson <emma.uddesson@gmail.com>
2018-07-30 22:42:45 +0200

    Draft for displaying reminders stored with AsyncStorage

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, AsyncStorage } from 'react-native';
 import { Paragraph } from './common/TextFormats';
 
+
 export default class List extends Component {
+  state = {
+    reminders: null,
+  }
+
+
+  async loadReminders() {
+    // TODO: Error handling
+    await AsyncStorage.getAllKeys()
+    .then((keys) => {
+      this.setReminders(keys);
+    })
+  }
+
+  async setReminders(keys) {
+    let reminders = [];
+    await AsyncStorage.multiGet(keys)
+    .then((result) => {
+      result.map((result, i, items) => {
+        let singleReminder = JSON.parse(items[i][1])
+        reminders.push(singleReminder)
+      });
+    })
+    this.setState({reminders})
+  }
+
+  componentDidMount(){
+    this.loadReminders();
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <Paragraph>Reminder</Paragraph>
-        <Paragraph>Reminder</Paragraph>
-        <Paragraph>Reminder</Paragraph>
-        <Paragraph>Reminder</Paragraph>
-        <Paragraph>Reminder</Paragraph>
-        <Paragraph>Reminder</Paragraph>
+        <FlatList
+          data={this.state.reminders}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <Paragraph>{`${item.text} at ${item.time}`}</Paragraph>}
+        />
       </View>
     );
   }
