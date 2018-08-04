9523460e0d48b05ae9258b987e4ccdbe994549ac
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-04 22:24:26 +0200

    Add small print text format

--- src/components/common/TextFormats.js
+++ src/components/common/TextFormats.js
@@ -13,14 +13,25 @@ export const Paragraph = ({ style, ...props }) => {
   );
 };
 
+export const SmallPrint = ({ style, ...props }) => {
+  return (
+    <Text style={[styles.smallprint, style]} {...props} />
+  );
+};
+
 const styles = StyleSheet.create({
   h1: {
-    fontSize: 17,
+    fontSize: 16,
     color: '#fff',
     fontWeight: 'bold',
   },
   paragraph: {
-    fontSize: 15,
+    fontSize: 14,
     color: '#2d2d2d',
-  }
+  },
+  smallprint: {
+    marginTop: 2,
+    color: 'rgba(0, 0, 0, 0.5)',
+    fontSize: 13,
+  },
 });
