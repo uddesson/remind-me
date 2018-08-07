888f6d9ab59734404289f25a040bbf09ccc4aaef
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-07 22:04:58 +0200

    Add bottom padding to avoid 'bounce-up' effect

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ export default class Main extends Component {
     return (
       <>
         <AddModal />
-        <View>
+        <View style={{paddingBottom: 120}}>
           <Header />
           <List />
         </View>
