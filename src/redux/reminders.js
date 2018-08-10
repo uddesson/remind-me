9545838d8304da12b87cc8342f9cfcc638266ab5
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-10 22:46:35 +0200

    Add support for selecting and deselecting reminder
    Used to update content of a single reminder

--- src/redux/reminders.js
+++ src/redux/reminders.js
@@ -1,15 +1,16 @@
 // Acions
-const DELETE_ALL_REMINDERS = 'remindme/reminders/DELETE_ALL_REMINDERS';
-
 const ADD_REMINDER = 'remindme/reminders/ADD_REMINDER';
-
 const UPDATE_REMINDERS = 'remindme/reminders/UPDATE_REMINDERS';
+const SELECT_REMINDER = 'remindme/reminders/SELECT_REMINDER';
+const DESELECT_REMINDER = 'remindme/reminders/DESELECT_REMINDER';
+const DELETE_ALL_REMINDERS = 'remindme/reminders/DELETE_ALL_REMINDERS';
 const TOGGLE_MODAL = 'remindme/reminders/TOGGLE_MODAL';
 
 // Reducer
 const INITIAL_STATE = {
   showModal: false,
   reminders: [],
+  selectedReminder: {},
 };
 
 export const remindersReducer = (state: any = INITIAL_STATE, action) => {
@@ -29,11 +30,22 @@ export const remindersReducer = (state: any = INITIAL_STATE, action) => {
         ...state,
         reminders: [],
       }
-    case TOGGLE_MODAL:
+    case SELECT_REMINDER:
       return {
         ...state,
-        showModal: !state.showModal,
+        selectedReminder: action.payload,
+        showModal: true,
       }
+    case DESELECT_REMINDER:
+    return {
+      ...state,
+      selectedReminder: {},
+    }
+    case TOGGLE_MODAL:
+    return {
+      ...state,
+      showModal: !state.showModal,
+    }
     default:
       return state;
   }
@@ -44,4 +56,6 @@ export const remindersReducer = (state: any = INITIAL_STATE, action) => {
 export const addReminder = (reminder) => ({ type: ADD_REMINDER, payload: reminder });
 export const updateReminders = (reminders) => ({ type: UPDATE_REMINDERS, payload: reminders });
 export const deleteAllReminders = () => ({ type: DELETE_ALL_REMINDERS })
+export const selectReminder = (reminder) => ({ type: SELECT_REMINDER, payload: reminder });
+export const deselectReminder = () => ({ type: DESELECT_REMINDER });
 export const toggleModal = () => ({ type: TOGGLE_MODAL });
