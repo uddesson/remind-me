dcff7eb119e8999ebe573329e29505554f085e94
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-13 15:29:52 +0200

    Remove temporary delete all-action in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,19 @@ import { HeaderOne } from './common/TextFormats';
 
 const mapStateToProps = state => ({ showModal: state.reminders.showModal });
 const mapDispatchToProps = dispatch => ({
-  triggerToggleModal: () => dispatch(toggleModal()),
-  triggerDeleteAllReminders: () => dispatch(deleteAllReminders())
+  triggerToggleModal: () => dispatch(toggleModal())
 });
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Header extends Component {
   render() {
-    const { triggerToggleModal, triggerDeleteAllReminders } = this.props;
+    const { triggerToggleModal } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.row}>
 
           <TouchableOpacity style={styles.col} onPress={() => {triggerToggleModal()}}>
-            <HeaderOne>Add reminder</HeaderOne>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.col} onPress={() => {triggerDeleteAllReminders()}}>
-            <HeaderOne>*Evil laugh*</HeaderOne>
+            <HeaderOne>Add reminder +</HeaderOne>
           </TouchableOpacity>
 
         </View>
@@ -37,7 +32,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingBottom: 30,
     paddingLeft: 20,
-    backgroundColor: 'tomato',
+    backgroundColor: '#475881',
   },
   row: {
     width: '100%',
